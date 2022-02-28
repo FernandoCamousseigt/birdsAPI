a18f88262401bea96a625f5c89d4c61c62c4073e
FernandoCamousseigt <82519981+FernandoCamousseigt@users.noreply.github.com>
2022-02-28 02:03:56 -0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+def request_get(url):
+    return json.loads(requests.get(url).text)
+
+if __name__ == '__main__':
+    response = request_get('https://aves.ninjas.cl/api/birds')
+    print(len(response))
---
+++ main.py
@@ -0,0 +1,19 @@
+from data import request_get
+import template as t
+
+def build_html(url):
+    response = request_get(url)
+    texto = ''
+    for resultados in response:
+
+        texto += t.elem_template.substitute(
+            titulo_es = resultados['name']['spanish'],
+            title_en = resultados['name']['english'],
+            url = resultados['images']['main']
+        )
+    return t.html_template.substitute(body = texto)
+
+if __name__ == '__main__':
+    html = build_html('https://aves.ninjas.cl/api/birds')
+    with open('output.html', 'w', encoding = 'utf-8') as f:
+        f.write(html)
